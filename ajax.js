40a0374f9f665a4c5c9b4e0bf6114e42437c574f
parimal438 <86233366+parimal438@users.noreply.github.com>
2021-08-16 21:33:04 +0600

    Add files via upload

---
+++ ajax.js
@@ -0,0 +1,9 @@
+function usersload(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json =>{
+    document.body.innerHTML=`<ul>${json.map
+    (u=>'<li>'+u.name+'</li>').join('')} </ul>`})
+
+    .catch(err=>console.log(err));
+}
